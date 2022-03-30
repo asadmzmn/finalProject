0dd1b5fc94115467e3a7096a84e6fb9f7f9968a2
asadmzmn <asadzaman494@gmail.com>
2022-03-30 14:28:29 -0700

    register created successfully

--- finalProject/user/views.py
+++ finalProject/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,4 +9,22 @@ def loginPage(request):
     return render(request, 'html/login.html')
 
 def registerPage(request):
-    return render(request, 'html/register.html')
+    if request.method == "GET":
+        return render(request, 'html/register.html')
+    elif request.method == "POST":
+        # print(request.POST)
+        userName = request.POST["user_name"]
+        password = request.POST["password"]
+        firstName = request.POST["first_name"]
+        lastName = request.POST["last_name"]
+        email = request.POST["email"]
+        
+        try:
+            User.objects.create_user(username = userName, password=password, email=email, first_name=firstName, last_name=lastName)
+            messages.success(request, "User created succesfully")
+            return redirect('loginpage')
+        except Exception as e:
+            messages.error(request, "Can't create user" + str(e))
+            return redirect("registerpage")
+
+
